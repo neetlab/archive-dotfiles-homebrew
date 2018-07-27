0b6ae9e2e2a0c7c915216b0d8183e722c32012d4
neet <n33t5hin@gmail.com>
2018-07-28 01:44:25 +0900

    Add fish configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+# Setting language
+set -x LC_ALL ja_JP.UTF-8
+
+# Setting ANSI colors to fishshell force
+set -x fish_color_autosuggestion black
+set -x fish_color_command        cyan
+set -x fish_color_comment        black
+set -x fish_color_end            green
+set -x fish_color_error          red
+set -x fish_color_param          cyan
+set -x fish_color_quote          green
+set -x fish_color_redirection    blue
