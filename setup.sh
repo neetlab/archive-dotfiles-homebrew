4351afaebfb47c87fdc3d7bda38df6f88dcd63e6
Ryo Igarashi <n33t5hin@gmail.com>
2020-07-20 13:04:10 +0900

    Add .macos

--- setup.sh
+++ setup.sh
@@ -1,8 +1,4 @@
 sudo sh -c "echo $(which fish) > /etc/shells"
 chsh -s $(which fish)
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
-defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false
-defaults delete -g ApplePressAndHoldEnabled 
+source ./.macos
