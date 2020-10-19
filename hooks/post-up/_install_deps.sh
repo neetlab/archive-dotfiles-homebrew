329299969edcf688c40b8c7256ba0a5d307c8b32
Ryo Igarashi <n33t5hin@gmail.com>
2020-10-19 19:50:08 +0900

    Use ASDF to manage Node.js version

--- hooks/post-up/_install_deps.sh
+++ hooks/post-up/_install_deps.sh
@@ -6,3 +6,7 @@ brew bundle insatll
 
 # Install global Node.js packages
 yarn
+
+# ASDF Node.js
+# https://github.com/asdf-vm/asdf-nodejs#install
+bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
