7c378a89936079b833e264e2dc425ac5c9b65532
Ryo Igarashi <n33t5hin@gmail.com>
2020-10-24 14:39:05 +0900

    Install rustup and GHC

--- hooks/post-up/_install_deps.sh
+++ hooks/post-up/_install_deps.sh
@@ -7,6 +7,14 @@ brew bundle insatll
 # Install global Node.js packages
 yarn
 
-# ASDF Node.js
+# ASDF plugins
+asdf plugin add nodejs
+asdf plugin add haskell
+asdf plugin add rust
+
+# Install ASDF
+asdf install
+
 # https://github.com/asdf-vm/asdf-nodejs#install
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
+
