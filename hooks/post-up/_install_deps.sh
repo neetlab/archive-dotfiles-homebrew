5a6498b4092b4a82bc36cec84520a2f1b14d4dbe
Ryo Igarashi <n33t5hin@gmail.com>
2020-10-29 01:04:52 +0900

    Add PHP setup

--- hooks/post-up/_install_deps.sh
+++ hooks/post-up/_install_deps.sh
@@ -12,9 +12,20 @@ asdf plugin add nodejs
 asdf plugin add haskell
 asdf plugin add rust
 
-# Install ASDF
-asdf install
-
+# Node.js setup
 # https://github.com/asdf-vm/asdf-nodejs#install
 bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
 
+# PHP setup
+# https://github.com/asdf-community/asdf-php/issues/61
+# https://donatstudios.com/MojaveMissingHeaderFiles
+export PKG_CONFIG_PATH="$(brew --prefix icu4c)/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="$(brew --prefix krb5)/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="$(brew --prefix libedit)/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="$(brew --prefix libxml2)/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="$(brew --prefix openssl)/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PATH="$(brew --prefix bison)/bin:$PATH"
+export CPATH="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include"
+
+# Install ASDF
+asdf install
