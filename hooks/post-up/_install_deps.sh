e914ec782f9d0e6edb3fbd2306af498f1319ef17
Ryo Igarashi <n33t5hin@gmail.com>
2022-09-18 10:10:36 +0900

    Remove key ring

--- hooks/post-up/_install_deps.sh
+++ hooks/post-up/_install_deps.sh
@@ -15,9 +15,5 @@ asdf plugin add golang
 asdf plugin add python
 asdf plugin add deno
 
-# Node.js setup
-# https://github.com/asdf-vm/asdf-nodejs#install
-bash -c '${ASDF_DATA_DIR:=$HOME/.asdf}/plugins/nodejs/bin/import-release-team-keyring'
-
 # Install ASDF
 asdf install
